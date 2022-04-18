6a9082ee77b571dad706199399f2f02712250d57
manimaran003 <manimaranc@dorustree.in>
2022-04-18 08:28:59 +0530

    changes-homepage

--- src/RouterComponent/HomePage.js
+++ src/RouterComponent/HomePage.js
@@ -32,46 +32,43 @@ const HomePage = () => {
                         </>)
                     }
                     <div className="row">
-                        <div className="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-2 col-xl-2">
                             <div className="card" style={{ width: "180px", height: "200px" }}>
                                 <button onClick={() => navigate("/reducerapp")} style={{ width: "180px", height: "200px" }}>
                                     useEffect and useReducer sample app
                                 </button>
                             </div>
                         </div>
-                    </div>
-                    <div className="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
-                        <div className="card" style={{ width: "180px", height: "200px" }}>
-                            <button onClick={() => navigate("/apireducer")} style={{ width: "180px", height: "200px" }}>
-                                use reducer api call train admin
-                            </button>
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-2 col-xl-2">
+                            <div className="card" style={{ width: "180px", height: "200px" }}>
+                                <button onClick={() => navigate("/apireducer")} style={{ width: "180px", height: "200px" }}>
+                                    use reducer api call train admin
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
-                        <div className="card" style={{ width: "180px", height: "200px" }}>
-                            <button onClick={() => navigate("/reduxapi")} style={{ width: "180px", height: "200px" }}>
-                                Redux sample app train booking
-                            </button>
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-2 col-xl-2">
+                            <div className="card" style={{ width: "180px", height: "200px" }}>
+                                <button onClick={() => navigate("/reduxapi")} style={{ width: "180px", height: "200px" }}>
+                                    Redux sample app train booking
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
-                        <div className="card" style={{ width: "180px", height: "200px" }}>
-                            <button onClick={() => navigate("/typeEvent")} style={{ width: "180px", height: "200px" }}>
-                                typeScript  use reducer sample app crud operation
-                            </button>
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-2 col-xl-2">
+                            <div className="card" style={{ width: "180px", height: "200px" }}>
+                                <button onClick={() => navigate("/typeEvent")} style={{ width: "180px", height: "200px" }}>
+                                    typeScript  use reducer sample app crud operation
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
-                        <div className="card" style={{ width: "180px", height: "200px" }}>
-                            <button onClick={() => navigate("/BikeRedux")} style={{ width: "180px", height: "200px" }}>
-                                typeScript Redux Bike store app
-                            </button>
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-2 col-xl-2">
+                            <div className="card" style={{ width: "180px", height: "200px" }}>
+                                <button onClick={() => navigate("/BikeRedux")} style={{ width: "180px", height: "200px" }}>
+                                    typeScript Redux Bike store app
+                                </button>
+                            </div>
                         </div>
                     </div>
 
-                    <div>
-
-                    </div>
                 </div>
             </div>
         </>
